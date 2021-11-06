e2e519589b4d0adddef6509fe169f9d4454aa1a6
pangoleaf <mascot26@tuta.io>
2021-11-06 14:55:17 +0000

    styling added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const initialsForm = document.querySelector('.initials__form');
-const initialsBox = document.querySelector('.initials__box');
+const initialsBox = document.querySelector('.input__box');
 const wordsBox = document.querySelector('.words');
 
 const isAlpha = letter => /[a-zA-Z]/.test(letter);
 const choose = array => array[Math.floor(Math.random() * array.length)];
 const capitalise = word => word[0].toUpperCase() + word.slice(1);
+
+const fadeTransition = (div, newValue) => {
+  div.classList.add('hidden');
+  setTimeout(() => {
+      div.textContent = newValue;
+      div.classList.remove('hidden');
+  }, 200);
+}
 
 const getWords = function (fromInput) {
   return fromInput
@@ -20,5 +28,6 @@ const getWords = function (fromInput) {
 
 initialsForm.addEventListener('submit', e => {
   e.preventDefault();
-  wordsBox.textContent = getWords(initialsBox.value)
+  // wordsBox.textContent = getWords(initialsBox.value);
+  fadeTransition(wordsBox, getWords(initialsBox.value));
 });
